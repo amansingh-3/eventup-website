cc0557527dd57bf2451f89a787047db20c509f9c
Amansingh024 <amansingh213as@gmail.com>
2024-04-26 14:02:49 +0530

    Server.js Update

--- Backend/server.js
+++ Backend/server.js
@@ -1,90 +1,49 @@
 const express = require("express");
-const mysql = require("mysql");
+const { json } = require("express");
+const { createConnection } = require("mysql");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-const db = mysql.createConnection({
+const db = createConnection({
   host: "localhost",
   user: "root",
   password: "root",
   database: "eventup",
 });
 
-app.get("/user", (req, res) => {
-  console.log("tbl user");
-
-  const tblUsers_Query = "SELECT * FROM tbl_users";
-  console.log(db);
-  console.log(tblUsers_Query);
-  db.query(tblUsers_Query, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(data);
-  });
-
-  //   db.connect(function (err) {
-  //     if (err) throw err;
-  //     console.log("Connected!", db);
-
-  //   });
-
-  //   const tblUsers_Query = "SELECT * FROM tbl_users";
-  //   console.log(tblUsers_Query);
-  //   console.log(db);
-  //   db.query(sql, (err, data) => {
-  //     if (err) return res.json(err);
-  //     return res.json(data);
-  //   });
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
 });
 
-app.listen(8081, () => {
+app.post("/contactus", (req, res) => {
+  console.log("Inside Link");
+  const { email, fname, lname, phone, State, textarea } = req.body;
+
+  if (!email || !fname || !lname || !phone) {
+    console.error("Error: Missing required fields");
+    res.status(400).send("Missing required fields");
+    return;
+  }
+
+  const sql =
+    "INSERT INTO `tbl_users`( `First Name`, `Last Name`, `emailaddress`, `Phone`, `State`, `Query`)VALUES (?, ?, ?, ?, ?, ? )";
+  const values = [fname, lname, email, phone, State, textarea];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error("Error inserting data:", err);
+      res.status(500).send("Error inserting data");
+      return;
+    }
+    console.log("Data inserted:", result);
+    res.status(200).send("Data inserted");
+  });
+});
+
+app.listen(3000, () => {
   console.log("listening");
 });
-
-// // Import required modules
-// const express = require("express");
-// const mysql = require("mysql");
-
-// // Create an Express.js application
-// const app = express();
-// const port = 3000;
-
-// // Create MySQL connection pool
-// const pool = mysql.createPool({
-//   connectionLimit: 10,
-//   host: "localhost",
-//   user: "root",
-//   password: "root", // Replace with your MySQL password
-//   database: "eventup", // Replace with your MySQL database name
-// });
-
-// // Define a route to fetch data from MySQL database
-// app.get("/users", (req, res) => {
-//   // Get a connection from the pool
-//   pool.getConnection((err, connection) => {
-//     if (err) {
-//       console.error("Error connecting to MySQL:", err);
-//       return res.status(500).json({ error: "Internal server error" });
-//     }
-
-//     // Use the connection to execute a query
-//     connection.query("SELECT * FROM tbl_users", (err, results) => {
-//       // Release the connection back to the pool
-//       connection.release();
-
-//       if (err) {
-//         console.error("Error executing MySQL query:", err);
-//         return res.status(500).json({ error: "Internal server error" });
-//       }
-
-//       // Return the results as JSON
-//       res.json(results);
-//     });
-//   });
-// });
-
-// // Start the Express.js server
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
